48098fa05ce3fb0165e2e49aa4dd2e38783386a6
scheuchzer <thomas.scheuchzer@gmx.net>
2014-06-09 13:41:28 +0200

    Duplicates check test added. resolves #1

--- smarkdown-war/src/test/java/com/ja/smarkdown/preprocessing/MarkdownPreprocessorTest.java
+++ smarkdown-war/src/test/java/com/ja/smarkdown/preprocessing/MarkdownPreprocessorTest.java
@@ -2,7 +2,10 @@ package com.ja.smarkdown.preprocessing;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
+
+import java.io.ByteArrayInputStream;
 
 import javax.servlet.ServletContext;
 
@@ -12,13 +15,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.ja.smarkdown.model.ResourceInfo;
+import com.ja.smarkdown.model.config.Location;
+import com.ja.smarkdown.model.config.PagesConfiguration;
+import com.ja.smarkdown.model.config.SmarkdownConfiguration;
+
 @RunWith(MockitoJUnitRunner.class)
 public class MarkdownPreprocessorTest {
 
 	@Mock
 	private ServletContext servletContext;
+	@Mock
+	private PagesConfiguration pagesConfiguration;
+	@Mock
+	private SmarkdownConfiguration configuration;
 	@InjectMocks
-	private MarkdownPreprocessor processor = new MarkdownPreprocessor();
+	private final MarkdownPreprocessor processor = new MarkdownPreprocessor();
 
 	@Test
 	public void testProcessInternalLink() {
@@ -79,5 +91,21 @@ public class MarkdownPreprocessorTest {
 				"lorem ipsum ![alt](http://www.exampled.com/foo.png)");
 		assertThat(actual,
 				is("lorem ipsum ![alt](http://www.exampled.com/foo.png)"));
+	}
+
+	@Test
+	public void testDuplicates() {
+		doReturn(pagesConfiguration).when(configuration).getPages();
+		doReturn(true).when(pagesConfiguration).isCheckForDuplicates();
+		final ResourceInfo resourceInfo = new ResourceInfo("foo.md",
+				Location.create("foo:/"), new ByteArrayInputStream(
+						"test".getBytes()));
+		resourceInfo.getOverridden().add(
+				new ResourceInfo("foo.md", Location.create("bar:/"), null));
+		resourceInfo.getOverridden().add(
+				new ResourceInfo("foo.md", Location.create("zar:/"), null));
+		final String actual = processor.process("foo.html", resourceInfo);
+		assertTrue(actual.contains("alert"));
+		assertTrue(actual.endsWith("test"));
 	}
 }
