1004b932fbc6d74bf3715347261732f0886aa715
scheuchzer <thomas.scheuchzer@gmx.net>
2014-06-29 16:33:22 +0200

    Project split. resolves #7

--- smarkdown-war-servlet-container/runTomcat7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Starts Tomcat7 on port 9000
-#
-mvn clean verify org.codehaus.cargo:cargo-maven2-plugin:run \
-	-Dcargo.maven.containerId=tomcat7x \
-	-Dcargo.servlet.port=9000 \
-	-Dcargo.maven.containerUrl=http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.54/bin/apache-tomcat-7.0.54.zip
-	-DskipTests
